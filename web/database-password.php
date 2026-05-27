19d72fa4c7f5b075afbe484e90b376c98a3568c9
PencilCore <pencilzyl@gmail.com>
2026-05-27 18:51:22 +0800

    chore: 移除旧版PureCopy web项目所有代码和资源
    本次提交删除了原web目录下的所有后端PHP脚本、前端页面、JS代码、静态图片资源以及依赖库，同时新增了slint子模块配置，并更新了trae的监控状态文件。该操作是项目重构的一部分，将原有PHP+前端的纯网页剪贴板项目迁移到基于Slint的桌面应用架构。

--- web/database-password.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $db_servername="";
-    $db_username="";
-    $db_password="";
-?>
--- web/js/cursor-tracker.js
+++
@@ -1,9 +0,0 @@
-function cursorTracker(trackobj_id,parameter){
-    let trackObj = document.getElementById(trackobj_id);
-    document.onmousemove = function(mousemoveevent){
-    mousemoveevent = mousemoveevent || window.mousemoveevent;
-    let left = (mousemoveevent.clientX - (window.innerWidth / 2) )/parameter;
-    let top = (mousemoveevent.clientY - (window.innerWidth / 2) )/parameter;
-    trackObj.style.top = top + "px";
-    trackObj.style.left = left + "px";
-}}
